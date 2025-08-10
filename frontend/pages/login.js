42e011264ae53d8df035f484945b499b9618bca2
TomoChart <tomislavmartinovic01@gmail.com>
2025-08-10 17:56:30 +0200

    Update login.js

--- frontend/pages/login.js
+++ frontend/pages/login.js
@@ -1,3 +1,6 @@
 export default function Login() {
   return <div>Login Page</div>;
 }
+// unutar handleSubmit
+localStorage.setItem('session', JSON.stringify({ role:'superadmin', country:'HR' }));
+window.location.href = '/dashboard';
